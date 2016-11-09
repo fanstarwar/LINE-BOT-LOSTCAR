d43a93dd088bce9ace98954ea702ee67b9f4ec54
Pariwat Chotkaew <ButaO.OKwang@192.168.1.35>
2016-11-09 09:44:27 +0700

    ddd

--- bot.php
+++ bot.php
@@ -15,6 +15,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+                        /*
 			$messages =[ 
                                 [
                                     'type' => 'text',
@@ -28,8 +29,18 @@ if (!is_null($events['events'])) {
                                 ]
                                
 			];
-
+                        */
                         
+                        $messages =[ 
+                                [
+                                    'type' => 'text',
+                                    'text' => 'ข้อความที่ท่านส่งมา '.$text
+                                ],
+                                [
+                                    'type' => 'text',
+                                    'text' => 'ขอบคุณสำหรับการทดสอบ'
+                                ]
+                        ];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
